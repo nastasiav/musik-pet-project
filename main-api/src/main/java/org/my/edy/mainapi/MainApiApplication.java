22cb98c02209245572c405d26234c8fc1c5ea7b2
nastasiav <baeva.asya@gmail.com>
2026-04-12 17:34:48 +0300

    Init

---
+++ main-api/src/main/java/org/my/edy/mainapi/MainApiApplication.java
@@ -0,0 +1,13 @@
+package org.my.edy.mainapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApiApplication.class, args);
+	}
+
+}
---
+++ main-api/src/test/java/org/my/edy/mainapi/MainApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.my.edy.mainapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
