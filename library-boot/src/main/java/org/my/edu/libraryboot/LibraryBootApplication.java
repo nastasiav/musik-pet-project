64af7c8593987458781c91b5ae72f0264607acab
nastasiav <baeva.asya@gmail.com>
2026-04-12 17:49:17 +0300

    Init GraphQl v1

---
+++ library-boot/src/main/java/org/my/edu/libraryboot/LibraryBootApplication.java
@@ -0,0 +1,13 @@
+package org.my.edu.libraryboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryBootApplication {
+
+    static void main(String[] args) {
+		SpringApplication.run(LibraryBootApplication.class, args);
+	}
+
+}
