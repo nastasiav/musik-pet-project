52d6fea8de054d3de12647ca0df77761d4b69108
nastasiav <baeva.asya@gmail.com>
2026-04-12 18:46:55 +0300

    Init Player on Tomcat

---
+++ player-tomcat/src/main/java/org/my/edu/player_tomcat/PlayerTomcatApplication.java
@@ -0,0 +1,13 @@
+package org.my.edu.player_tomcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerTomcatApplication {
+
+    static void main(String[] args) {
+		SpringApplication.run(PlayerTomcatApplication.class, args);
+	}
+
+}
---
+++ player-tomcat/src/test/java/org/my/edu/player_tomcat/PlayerTomcatApplicationTests.java
@@ -0,0 +1,13 @@
+package org.my.edu.player_tomcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayerTomcatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
