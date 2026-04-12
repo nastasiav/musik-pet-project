a730f0a20dd640f6ee2b6cd9151e6544732deb4a
nastasiav <baeva.asya@gmail.com>
2026-04-12 18:39:09 +0300

    Init Webfux

---
+++ recommend-webflux/src/main/java/org/my/edu/recommend_webflux/RecommendWebfluxApplication.java
@@ -0,0 +1,13 @@
+package org.my.edu.recommend_webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendWebfluxApplication {
+
+    static void main(String[] args) {
+		SpringApplication.run(RecommendWebfluxApplication.class, args);
+	}
+
+}
---
+++ recommend-webflux/src/test/java/org/my/edu/recommend_webflux/RecommendWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package org.my.edu.recommend_webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
