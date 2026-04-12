a20dada594591dff1c4e7f3eaa9c24c6b67b5e2a
nastasiav <baeva.asya@gmail.com>
2026-04-12 18:11:14 +0300

    Init GraphQl v2

---
+++ library-clean/src/main/java/org/my/edy/Main.java
@@ -0,0 +1,34 @@
+package org.my.edy;
+
+import org.eclipse.jetty.ee10.servlet.ServletContextHandler;
+import org.eclipse.jetty.server.Server;
+import org.my.edy.exceprions.MyServerException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+    static void main() {
+        int port = 8102;
+        Server server = new Server(port);
+
+        ServletContextHandler context = new ServletContextHandler();
+        context.setContextPath("/");
+        server.setHandler(context);
+
+        try {
+            server.start();
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new MyServerException("Server Jetty cannot started");
+        }
+        System.out.println("Server started on port 8102");
+        try {
+            server.join();
+        } catch (InterruptedException e) {
+            log.error(e.getMessage());
+            throw new MyServerException("Server Jetty error join");
+        }
+    }
+}
---
+++ library-clean/src/main/java/org/my/edy/exceprions/MyServerException.java
@@ -0,0 +1,7 @@
+package org.my.edy.exceprions;
+
+public class MyServerException extends RuntimeException {
+    public MyServerException(String message) {
+        super(message);
+    }
+}
