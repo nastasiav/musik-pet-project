28b3a86d9abfb1f70c638cc3916690f7cc869294
nastasiav <baeva.asya@gmail.com>
2026-04-21 22:08:20 +0300

    add playlist boot

---
+++ playlist-jetty-boot/src/test/java/org/my/edu/playlist_jetty_boot/PlaylistJettyBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.my.edu.playlist_jetty_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylistJettyBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
