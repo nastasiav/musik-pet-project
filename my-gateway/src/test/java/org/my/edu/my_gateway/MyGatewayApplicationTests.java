93f4c607ab48b24e49de76e073b619e0fff6f25b
nastasiav <baeva.asya@gmail.com>
2026-04-21 20:51:42 +0300

    add GW

---
+++ my-gateway/src/test/java/org/my/edu/my_gateway/MyGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.my.edu.my_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
